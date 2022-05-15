471d1645baad3da48154b551dbb9a20b900136b1
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-05-15 16:41:02 -0300

    chore: change all styled component to use tailwind.css stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import GlobalStyle from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle/>
       <Component {...pageProps} />
     </>
   )
