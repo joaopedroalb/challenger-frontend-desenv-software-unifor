06761aa8660abd069b29ae46820c6299bd5cd988
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-05-18 17:46:39 -0300

    feat: create card component and update home page

---
+++ src/components/CardGitPerson.tsx
@@ -0,0 +1,26 @@
+
+type CardGitPersonProps = {
+    name:string
+    profile:string
+    enroll:string
+}
+
+export default function CardGitPerson({name,profile,enroll}:CardGitPersonProps) {
+  return (
+    <a href={`https://github.com/${profile}`}>
+        <div className={`flex flex-col items-center p-8 transition-colors duration-200 transform 
+                        cursor-pointer group hover:bg-blue-600 rounded-xl border-2 border-solid border-white
+                        max-w-[15rem] text-center h-80 hover:scale-105 transition-transform`}>
+            <img    className="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300 grayscale-[100%] group-hover:grayscale-0" 
+                    src={`https://github.com/${profile}.png`} 
+                    alt="" />
+            <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white group-hover:text-white">{name}</h1>
+            <p className="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">{enroll}</p>
+            
+            <div className="flex mt-3 -mx-2">
+                
+            </div>
+        </div>
+    </a>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,47 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import CardGitPerson from "../components/CardGitPerson";
+
+const PERSONS_LIST = [
+  {
+    name:"João Pedro Albuquerque",
+    profile:"joaopedroalb",
+    enroll:"1710589"
+  },
+  {
+    name:"Patrick Martins",
+    profile:"pattrickx",
+    enroll:"1711010"
+  },
+  {
+    name:"Edson Holanda",
+    profile:"EdsonHTJ",
+    enroll:"1710590"
+  }
+]
 
 const Home: NextPage = () => {
   return (
-    <div className="w-screen min-h-screen flex items-center justify-center bg-black">
+    <div className="w-screen min-h-screen flex flex-col items-center justify-start gap-8 pt-8 bg-black text-white">
+      
+      <div className={`w-full flex flex-col justify-center items-center gap-8 flex-wrap`}>
+        <h1 className={`text-7xl font-sans font-bold`}>
+          Grupo
+        </h1>
+        <div className={`w-full flex justify-center gap-4 flex-wrap`}>
+        {PERSONS_LIST.map(person=>{
+          return <CardGitPerson
+                    profile={person.profile}
+                    name={person.name}
+                    enroll={person.enroll}
+                    key={person.enroll}
+                  />
+        })}
+        </div>
+      </div>
+      
       <Link href="/dashboard">
-        <button className="bg-white text-black p-4 rounded-xl">Go to dashboard</button>
+        <button className="bg-white text-black p-4 rounded-xl hover:scale-[1.02] transition-transform">Go to challenger</button>
       </Link>
     </div>
   )
